082e0d199f38d691a9fed4de075762b12fc034d5
2sdat <aidanfc97@gmail.com>
2019-02-21 00:04:36 -0500

    Completed NameBag functionality

--- AccountManager/src/AccountManager/NameBag.java
+++ AccountManager/src/AccountManager/NameBag.java
@@ -2,54 +2,67 @@ package AccountManager;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Stream;
+import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class NameBag {
 	private String boysNamesFile;
 	private String girlsNamesFile;
 	private String lastNamesFile;
-	private String[] boysNames;
-	private String[] girlsNames;
-	private String[] lastNames;
+	private Pattern namePattern;
+	private ArrayList<String> boysNames;
+	private ArrayList<String> girlsNames;
+	private ArrayList<String> lastNames;
 	
-	public NameBag(String boysNamesFile, String girlsNamesFile, String lastNamesFile) {
+	public NameBag(String boysNamesFile, String girlsNamesFile, String lastNamesFile) throws IOException {
 		this.boysNamesFile = boysNamesFile;
 		this.girlsNamesFile = girlsNamesFile;
 		this.lastNamesFile = lastNamesFile;
+		namePattern = Pattern.compile("[A-z][a-z]+");
 		loadNames();
 	}
-
-	private void loadNames() {
-		boysNames = loadBoysNames();
-		girlsNames = loadGirlsNames();
-		lastNames = loadLastNames();
+	
+	private String loadTextFileToString(String pathToFile) throws IOException {
+		ArrayList<String> lines = new ArrayList<String>();
+		BufferedReader br = new BufferedReader(new FileReader(pathToFile));
+		String curLine;
+		while((curLine = br.readLine()) != null) {
+			lines.add(curLine);
+		}
+		br.close();
+		return lines.toString();
 	}
 	
-	private String[] loadLastNames() {
-		List<String> lastNames = new ArrayList<String>();
-		try {
-			FileReader fr = new FileReader(lastNamesFile);
-			BufferedReader br = new BufferedReader(fr);
-			String line = br.readLine();
-			while(line != null) {
-				lastNames.add(line);
-				line = br.readLine();
-			}
-		} finally {
-			fr.close();
+	private ArrayList<String> loadNamesFromFile(String pathToFile) throws IOException {
+		ArrayList<String> list = new ArrayList<String>();
+		Matcher nameMatcher = namePattern.matcher(loadTextFileToString(pathToFile));
+		while(nameMatcher.find()) {
+			list.add(nameMatcher.group(0));
 		}
-		
-		return null;
+		return list;
+	}
+	
+	private void loadNames() throws IOException {
+		lastNames = loadNamesFromFile(lastNamesFile);
+		girlsNames = loadNamesFromFile(girlsNamesFile);
+		boysNames = loadNamesFromFile(boysNamesFile);
 	}
 
-	private String[] loadGirlsNames() {
-		return null;
+	public String getBoysName() {
+		Random rand = new Random();
+		return boysNames.get(rand.nextInt(boysNames.size()));
 	}
 
-	private String[] loadBoysNames() {
-		return null;
+	public String getGirlsName() {
+		Random rand = new Random();
+		return girlsNames.get(rand.nextInt(girlsNames.size()));
 	}
 
+	public String getLastName() {
+		Random rand = new Random();
+		return lastNames.get(rand.nextInt(lastNames.size()));
+	}
 }
--- AccountManager/testsrc/AccountManager/NameBagTest.java
+++ AccountManager/testsrc/AccountManager/NameBagTest.java
@@ -9,12 +9,12 @@ import org.junit.jupiter.api.Test;
 
 class NameBagTest {
 	private NameBag nameBag;
-	private final String BOYSNAMESFILE = "boys_names.txt";
-	private final String GIRLSNAMESFILE = "girls_names.txt";
-	private final String LASTNAMESFILE = "last_names.txt";
+	private final String BOYSNAMESFILE = "/home/aidan/Documents/cse248/AccountManager/src/AccountManager/boys_names.txt";
+	private final String GIRLSNAMESFILE = "/home/aidan/Documents/cse248/AccountManager/src/AccountManager/girls_names.txt";
+	private final String LASTNAMESFILE = "/home/aidan/Documents/cse248/AccountManager/src/AccountManager/last_names.txt";
 	
-	@Test
-	void testLoadFile() {
+	@BeforeEach
+	void setup() {
 		try {
 			nameBag = new NameBag(BOYSNAMESFILE, GIRLSNAMESFILE, LASTNAMESFILE);
 		} catch(IOException e) {
@@ -23,18 +23,26 @@ class NameBagTest {
 	}
 	
 	@Test
-	void testEmitBoysFirstName() {
-		fail("Not implemented.");
+	void testGetBoysName() {
+		for(int i = 0; i < 3000; i++) {
+			assertEquals(true, nameBag.getBoysName().length() > 0);
+			assertEquals(false, nameBag.getBoysName().contains(" "));
+		}
 	}
 	
 	@Test
-	void testEmitGirlsFirstName() {
-		fail("Not implemented.");
+	void testGetGirlsName() {
+		for(int i = 0; i < 3000; i++) {
+			assertEquals(true, nameBag.getGirlsName().length() > 0);
+			assertEquals(false, nameBag.getGirlsName().contains(" "));
+		}
 	}
 	
 	@Test
-	void testEmitLastName() {
-		fail("Not implemented.");
+	void testGetLastName() {
+		for(int i = 0; i < 3000; i++) {
+			assertEquals(true, nameBag.getLastName().length() > 0);
+			assertEquals(false, nameBag.getLastName().contains(" "));
+		}
 	}
-
 }
